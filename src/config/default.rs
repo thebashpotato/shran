200b1becaaa4719247b350df1c95fa9ea5e74dca
Matt Williams <mattinzhuhai@outlook.com>
2021-11-08 16:02:31 -0800

    :octocat: Added config module, with default values, and tests

---
+++ src/config/default.rs
@@ -0,0 +1,76 @@
+use std::env;
+
+pub enum ShranFile {
+    GhToken,
+    BitcoinBuildLog,
+    BitcoinBuildConfig,
+}
+
+pub struct ShranDefault;
+
+impl<'a> ShranDefault {
+    pub const PROGNAME: &'a str = "shran";
+    pub const GH_TOKEN_FILENAME: &'a str = "gh.yaml";
+    pub const BUILD_CONFIG_FILENAME: &'a str = "bitcoin-build.yaml";
+    pub const BUILD_LOG_FILENAME: &'a str = "bitcoin-build.log";
+
+    #[inline]
+    pub fn config_dir() -> String {
+        format!("{}/.config/{}", env!("HOME"), Self::PROGNAME)
+    }
+
+    #[inline]
+    pub fn build_dir() -> String {
+        String::from(env::current_dir().unwrap().to_str().unwrap())
+    }
+
+    #[inline]
+    pub fn forfile(file: ShranFile) -> String {
+        match file {
+            ShranFile::GhToken => format!("{}/{}", Self::config_dir(), Self::GH_TOKEN_FILENAME),
+            ShranFile::BitcoinBuildLog => {
+                format!("{}/{}", Self::build_dir(), Self::BUILD_LOG_FILENAME)
+            }
+            ShranFile::BitcoinBuildConfig => {
+                format!("{}/{}", Self::build_dir(), Self::BUILD_CONFIG_FILENAME)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{ShranDefault, ShranFile};
+
+    #[test]
+    fn test_build_dir() {
+        let expected: String = format!("{}", env!("CARGO_MANIFEST_DIR"));
+        assert_eq!(expected, ShranDefault::build_dir());
+    }
+
+    #[test]
+    fn test_build_dir_buildlog() {
+        let expected: String = format!(
+            "{}/{}",
+            env!("CARGO_MANIFEST_DIR"),
+            ShranDefault::BUILD_LOG_FILENAME
+        );
+        assert_eq!(expected, ShranDefault::forfile(ShranFile::BitcoinBuildLog))
+    }
+
+    #[test]
+    fn test_build_dir_buildconfig() {
+        let expected: String = format!(
+            "{}/{}",
+            env!("CARGO_MANIFEST_DIR"),
+            ShranDefault::BUILD_CONFIG_FILENAME
+        );
+        assert_eq!(expected, ShranDefault::forfile(ShranFile::BitcoinBuildConfig))
+    }
+
+    #[test]
+    fn test_shran_config_dir() {
+        let expected: String = format!("{}/.config/{}", env!("HOME"), ShranDefault::PROGNAME);
+        assert_eq!(expected, ShranDefault::config_dir());
+    }
+}
---
+++ src/config/mod.rs
@@ -0,0 +1,3 @@
+pub use default::{ShranFile, ShranDefault};
+
+pub mod default;
