1d3baa151c59db45a592341e494617afb69ed13a
Matt Williams <mattltf@protonmail.com>
2022-06-21 14:45:04 -0600

    :godmode: #10 clap is ported to v3 using the builder api

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,22 @@ mod cli;
 mod config;
 mod error;
 
+pub use cli::commands::{ActiveCommand, SubCommandName};
 pub use cli::Cli;
 pub use config::{ShranDefault, ShranFile};
 pub use error::ShranError;
+
+fn run_generate(node_type: &String) {
+    println!("Generating build for: {}", node_type);
+}
+
+fn run_build(path: &String) {
+    println!("Build file path {}", path);
+}
+
+fn run_auth(token: &String) {
+    println!("Running auth code {}", token);
+}
 
 fn main() {
     let cli = Cli::new().unwrap_or_else(|error: ShranError| {
@@ -12,8 +25,17 @@ fn main() {
         std::process::exit(1);
     });
 
-    let ac = cli.active_command();
+    let ac: &ActiveCommand = cli.active_command();
 
-    println!("Subcommand: {}", ac.sub_command());
-    println!("Argument: {}", ac.arg());
+    if ac.sub_command() == SubCommandName::GENERATE {
+        run_generate(ac.arg());
+    }
+
+    if ac.sub_command() == SubCommandName::BUILD {
+        run_build(ac.arg());
+    }
+
+    if ac.sub_command() == SubCommandName::AUTH {
+        run_auth(ac.arg());
+    }
 }
